2df00adec2fd45f21cf0536e266cc2685ed66e76
Josie Hann <125931027+JosieHann@users.noreply.github.com>
2023-08-14 18:35:27 -0400

    initial code created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      Dictionary
+    </div>
+  );
+}
+
+export default App;
