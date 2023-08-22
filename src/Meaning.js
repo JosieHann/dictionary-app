701862cc5dcd1608a6d39df4d08e05b714720116
Josie Hann <125931027+JosieHann@users.noreply.github.com>
2023-08-22 19:17:17 -0400

    added synonym code

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import Synonyms from "./Synonyms"; 
 export default function Meaning(props) {
-  return (
-    <div>{props.meaning.definition}</div>
-  )
+  if (props.meaning) {
+    return (
+      <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        <div className="definition">{props.meaning.definition}</div>
+        <div className="example">{props.meaning.example}</div>
+
+        <Synonyms synonyms={props.meaning.synonyms} /> 
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
