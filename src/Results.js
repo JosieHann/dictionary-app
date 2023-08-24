cffcd2e1d91e133e450f436de4d97c3df1620cc0
Josie Hann <125931027+JosieHann@users.noreply.github.com>
2023-08-23 21:07:46 -0400

    updated results.js

--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,11 @@
 import React from "react";
-import Phonetics from "./Phonetics";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
-  if (props.definition) {
+  if (props.results) {
     return (
       <div className="Results">
-        <section>
-          <h2>{props.definition.word}</h2>
-          <Phonetics phonetic={props.definition.phonetics[0].text} /> 
-        </section>
-
-        {props.definition.meanings.map(function (meaning, index) { 
+        {props.results.meanings.map(function (meaning, index) { 
           return (
             <section key={index}>
               <Meaning meaning={meaning} />
