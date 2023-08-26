97215f0fbf492330baac5d34b47ab9e5081c88d3
Josie Hann <125931027+JosieHann@users.noreply.github.com>
2023-08-26 15:08:29 -0400

    added photos, and keyword

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
       <header className="App-header">
       </header>
       <main>
-        <Dictionary />
+      <Dictionary defaultKeyword="serial killer" />
       </main>
-    <footer className='App-footer'> <small>Coded by Josie Hann Hosted on <a href='https://extraordinary-pixie-e7bb51.netlify.app/'>Netlify</a> and open-sourced on <a href='https://github.com/JosieHann/dictionary-app'>Github</a></small></footer>
+    <footer className='App-footer'> <small>Coded by <a href='https://sitesiren-josie-portfolio.netlify.app/'> Josie Hann</a> Hosted on <a href='https://extraordinary-pixie-e7bb51.netlify.app/'>Netlify</a> and open-sourced on <a href='https://github.com/JosieHann/dictionary-app'>Github</a></small></footer>
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,51 +3,62 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
-
-
-export default function Dictionary () {
-    let [Keyword, setKeyword] = useState("scary movie");
-    let [results, setResults] = useState(null);
-    let [loaded, setLoaded] = useState(false);
-
-    function handleResponse(response) {
-     setResults(response.data);
+  function handleResponse(response) {
+    setResults(response.data[0]);
   }
-    function search () {
-       // documentation: https://dictionaryapi.dev/e
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${Keyword}`;
+function handlePexelsResponse(response) {
+console.log(response);
+}
+  function search() {
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
-    }
-  
-    function handleSubmit(event) {
-      event.preventDefault();
-      search();
-    }
-    function handleKeywordChange (event) {
-      setKeyword(event.target.value);
+    
+    let pexelsApiKey = "SwfANgqBQbjwnqVpq08nZzfU0I9prsg5ookWOHMxTSVc9SAWXMwBuQwD";
+    let pexelsApiUrl =  `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    axios.get(pexelsApiUrl, {headers: {"Authorization" : `Bearer${pexelsApiKey}`}}).then(handlePexelsResponse);
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   function load() {
-setLoaded(true);
-search();
+    setLoaded(true);
+    search();
   }
-    if (loaded) {
+
+  if (loaded) {
     return (
-        <div className="Dictionary">
-          <section> 
-            <h2>Word Dictionary 📚 Look It Up🔍</h2>
+      <div className="Dictionary">
+        <section>
+          <h2>Word Dictionary📚 Look It Up🔍</h2>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} autoFocus={true} placeholder="🔍" />
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
           <div className="hint">
-            suggested words:sunset, knife, flower, Agoraphobia... 
+            suggested words:blood, wine, killer, flower...
           </div>
-          </section>
-          <Results results={results} />
-        </div>
-    )
-} else {
-  load();
-  return "Loading";
-}
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
