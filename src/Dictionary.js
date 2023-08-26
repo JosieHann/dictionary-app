c807604ba26a79769e685ce56e409b80d7362323
Josie Hann <125931027+JosieHann@users.noreply.github.com>
2023-08-26 15:20:53 -0400

    added new functions, and photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,27 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
   }
-function handlePexelsResponse(response) {
-console.log(response);
-}
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
-    
-    let pexelsApiKey = "SwfANgqBQbjwnqVpq08nZzfU0I9prsg5ookWOHMxTSVc9SAWXMwBuQwD";
-    let pexelsApiUrl =  `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
-    axios.get(pexelsApiUrl, {headers: {"Authorization" : `Bearer${pexelsApiKey}`}}).then(handlePexelsResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "SwfANgqBQbjwnqVpq08nZzfU0I9prsg5ookWOHMxTSVc9SAWXMwBuQwD";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -51,10 +57,11 @@ console.log(response);
             />
           </form>
           <div className="hint">
-            suggested words:blood, wine, killer, flower...
+            suggested words: flower, wine, fight, ghosts...
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
