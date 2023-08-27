547066352519ec922e5d90078194fd258a9606c6
Josie Hann <125931027+JosieHann@users.noreply.github.com>
2023-08-27 19:02:40 -0400

    fixed code

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,6 +4,7 @@ import Synonyms from "./Synonyms";
 import "./Synonyms.css";
 
 
+
 export default function Meaning(props) {
   return (
     <div className="Meaning">
@@ -13,7 +14,7 @@ export default function Meaning(props) {
           <div key={index}>
             <div className="definition">{definition.definition}</div>
             <div className="example">{definition.example}</div>
-            <Synonyms synonyms={definition.synonyms} /> 
+            <Synonyms synonyms={props.meaning.definitions[1].synonyms} />
           </div>
         );
       })}
